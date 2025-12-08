ef224d29bb223b81642aa14796cb09435ce7d7f3
Sahil884 <sahiltiwari666@gmail.com>
2025-12-08 23:22:39 +0530

    refactor CORS configuration to use allowed origins array for better control

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,23 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173", // dev
+  "https://taskly-task-manager-icrv-9yvprhk97.vercel.app", // production frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
