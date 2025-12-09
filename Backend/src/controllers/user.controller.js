d20abc9a1ede7c1bc672b1618611d0c722aadc3c
Sahil884 <sahiltiwari666@gmail.com>
2025-12-10 00:56:07 +0530

    refactor: add sameSite attribute to cookie options in login and logout functions

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -105,6 +105,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
@@ -138,6 +139,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
