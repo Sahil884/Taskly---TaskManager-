4cf1533aa6bbcc227b611ab7cd1404ce399acea3
Sahil884 <sahiltiwari666@gmail.com>
2025-12-09 22:52:35 +0530

    refactor: add explicit handling for preflight requests in CORS configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -43,6 +43,14 @@ app.use(
 // ✅ Handle preflight requests explicitly
 // app.options("*", cors(corsOptions));
 
+app.options(
+  "*",
+  cors({
+    origin: "https://taskly-task-manager-icrv.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
