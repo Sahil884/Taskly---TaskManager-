3d30f6c01cfa2829549ee1a3d0b8c12f11de5d4c
Sahil884 <sahiltiwari666@gmail.com>
2025-12-10 01:01:46 +0530

    refactor: add sameSite attribute to cookie options in refreshAccessToken function

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -176,6 +176,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: "none",
     };
 
     const { accessToken, newRefreshToken } =
